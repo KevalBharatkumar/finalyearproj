f88a7a0e81fff8b7d6227d44a54021d44643cd55
Kev <keval012@hotmail.com>
2024-01-31 23:06:10 +0000

    s

---
+++ finalyearproj/src/pages/PartspageGames/PartspageGame2.js
@@ -0,0 +1,58 @@
+import "./Design.css"
+import Navbar from "./Navbar"
+
+export default function PartspageGame2(){ 
+
+    
+
+
+    return(
+        
+        <div>
+            <div class = "head">
+             <div class = "head">
+                    <p>PCParts</p>
+                </div>
+
+                <Navbar/>
+                    
+                    <div class = "box">
+
+                    <div class="hide" id = "hide1">
+                        <p>Hi</p>
+                    </div>
+
+                    </div>
+
+
+                    <div class = "box2">
+
+                    </div>
+
+
+                    <div class = "box3">
+
+                    </div>
+
+
+                    <div class = "box4">
+
+                    </div>
+
+
+                    <div class = "box5">
+
+                    </div>
+                
+
+
+            </div>
+        </div>
+        
+
+        
+        
+        
+    )
+    
+    }
